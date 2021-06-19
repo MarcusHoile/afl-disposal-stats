569eafb821137d231dfc8a6ccbe54ee2f83eb05f
marcushoile <marcus.hoile@gmail.com>
2021-06-19 15:35:17 +1000

    query for player data

---
+++ lib/player_stats/data.ex
@@ -0,0 +1,37 @@
+defmodule PlayerStats.Data do
+  import Ecto.Query
+  alias PlayerStats.{Repo, Schema}
+
+  def get_player(
+        %{first_name: first_name, last_name: last_name, team_name: team_name},
+        %{id: season_id} = _season \\ current_season()
+      ) do
+    from(p in Schema.Player,
+      join: ps in assoc(p, :player_seasons),
+      on: ps.season_id == ^season_id,
+      join: t in assoc(ps, :team),
+      on: like(t.name, ^"%#{team_name}%"),
+      where: p.first_name == ^first_name,
+      where: p.last_name == ^last_name
+    )
+    |> Repo.one()
+  end
+
+  def player_disposals(%{id: player_id} = _player, %{id: season_id} = _season \\ current_season()) do
+    from(gp in Schema.GamePlayer,
+      join: g in assoc(gp, :game),
+      join: ps in assoc(gp, :player_season),
+      on: ps.season_id == ^season_id,
+      where: gp.player_id == ^player_id,
+      select: %{
+        round: g.round,
+        disposals: gp.disposals
+      }
+    )
+    |> Repo.all()
+  end
+
+  defp current_season do
+    Repo.get_by(Schema.Season, year: Date.utc_today().year())
+  end
+end
