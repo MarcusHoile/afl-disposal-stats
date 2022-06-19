bb4b4a49217ee1286f79bcf98338294bd1163613
marcushoile <marcus.hoile@gmail.com>
2022-06-19 18:58:44 +1000

    player stat query for two teams

--- lib/player_stats.ex
+++ lib/player_stats.ex
@@ -20,21 +20,31 @@ defmodule PlayerStats do
 
   defp team_list_players(team_id, filter) do
     from(p in Schema.Player,
-      join: gp in assoc(p, :game_players),
-      where: gp.game_id in subquery(game_query(team_id, filter)),
-      preload: [game_players: gp]
+      join: gp in subquery(game_player_query(team_id, filter)),
+      on: gp.player_id == p.id,
+      distinct: p.id,
+      preload: [game_players: ^game_player_preload_query(filter)]
     )
   end
 
-  defp game_query(team_id, %{current_year: current_year, rounds: rounds}) do
-    from(g in Schema.Game,
+  defp game_player_query(team_id, %{current_year: current_year, rounds: rounds}) do
+    from(gp in Schema.GamePlayer,
+      join: g in assoc(gp, :game),
       join: t in assoc(g, :teams),
       join: s in assoc(g, :season),
-      where: t.id == ^team_id,
-      group_by: [g.id, t.id],
       limit: ^rounds,
       order_by: [desc: g.round],
-      select: g.id,
+      where: t.id == ^team_id,
+      where: s.year == ^current_year
+    )
+  end
+
+  defp game_player_preload_query(%{current_year: current_year, rounds: rounds}) do
+    from(gp in Schema.GamePlayer,
+      join: g in assoc(gp, :game),
+      join: s in assoc(g, :season),
+      limit: ^rounds,
+      order_by: [desc: g.round],
       where: s.year == ^current_year
     )
   end
--- test/player_stats_test.exs
+++ test/player_stats_test.exs
@@ -3,7 +3,7 @@ defmodule PlayerStatsTest do
   alias PlayerStats.Schema.{GamePlayer, Player}
 
   describe "list_players/1" do
-    test "filters players by team" do
+    test "filters players by single team" do
       player_season =
         :player_season
         |> insert()
@@ -21,9 +21,51 @@ defmodule PlayerStatsTest do
                    %GamePlayer{
                      disposals: 0,
                      goals: 0,
+                     handballs: 0,
+                     kicks: 3
+                   },
+                   %GamePlayer{
+                     disposals: 0,
+                     goals: 0,
                      handballs: 4,
                      kicks: 0
-                   },
+                   }
+                 ]
+               }
+             ] = PlayerStats.list_players(filter)
+    end
+
+    test "filters players by two teams" do
+      ps1 =
+        :player_season
+        |> insert()
+        |> with_game_player(handballs: 4)
+
+      team_season = build(:team_season, season: ps1.team_season.season)
+
+      ps2 =
+        :player_season
+        |> insert(team_season: team_season)
+        |> with_game_player(kicks: 3)
+
+      filter = %PlayerStats.Filter{
+        current_year: 2021,
+        team_ids: [ps1.team_season.team_id, ps2.team_season.team_id]
+      }
+
+      assert [
+               %Player{
+                 game_players: [
+                   %GamePlayer{
+                     disposals: 0,
+                     goals: 0,
+                     handballs: 4,
+                     kicks: 0
+                   }
+                 ]
+               },
+               %Player{
+                 game_players: [
                    %GamePlayer{
                      disposals: 0,
                      goals: 0,
