20875c445348f741a8766ef14dd0cbbadda460cb
marcushoile <marcus.hoile@gmail.com>
2023-03-25 16:26:06 +1100

    new season

--- lib/player_stats/crawler.ex
+++ lib/player_stats/crawler.ex
@@ -2,14 +2,14 @@ defmodule PlayerStats.Crawler do
   @moduledoc """
   Web crawler
   """
-  def crawl_season(round \\ 1, season \\ 2022, opts \\ default_options()) do
+  def crawl_season(round \\ 1, season \\ 2023, opts \\ default_options()) do
     url = "https://afltables.com/afl/seas/#{season}.html"
     opts = Keyword.merge(opts, season: season, round: round)
     {:ok, opts} = Crawler.crawl(url, opts)
     PlayerStats.Crawler.Monitor.start_link(opts)
   end
 
-  def crawl_url(url, round \\ 1, season \\ 2022, opts \\ default_options()) do
+  def crawl_url(url, round \\ 1, season \\ 2023, opts \\ default_options()) do
     opts = Keyword.merge(opts, season: season, round: round)
     {:ok, opts} = Crawler.crawl(url, opts)
     PlayerStats.Crawler.Monitor.start_link(opts)
